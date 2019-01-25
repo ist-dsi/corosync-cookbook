da8744b290221998beaf1d5e0f6b7585c451ba15
Simão Silva <simao.silva@tecnico.ulisboa.pt>
2019-01-25 16:13:58 +0000

    suport multiple platforms

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,18 +7,18 @@
 require 'spec_helper'
 
 describe 'corosync::default' do
-  context 'When using default recipe on debian' do
-    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'debian', version: '9.6').converge(described_recipe) }
+  platforms = {
+    'debian' => '9.6',
+    'centos' => '7.6.1804',
+    'ubuntu' => '18.04',
+  }
+  platforms.each do |platform, _value|
+    context 'When using default recipe on debian' do
+      let(:chef_run) { ChefSpec::ServerRunner.new(platform, version).converge(described_recipe) }
 
-    it 'installs package "corosync"' do
-      expect(chef_run).to install_package('corosync')
-    end
-  end
-  context 'When using default recipe on centos' do
-    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'centos', version: '7.6.1804').converge(described_recipe) }
-
-    it 'installs package "corosync"' do
-      expect(chef_run).to install_package('corosync')
+      it 'installs package "corosync"' do
+        expect(chef_run).to install_package('corosync')
+      end
     end
   end
 end
